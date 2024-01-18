7188ab5e8ec8ccd8cd8be093f2994572b736fa17
gazalssr <94488523+gazalssr@users.noreply.github.com>
2024-01-18 21:20:07 +0000

    added new datasets and changed the herdnet_finetuning.py codes

--- herdnet_finetuning.py
+++ herdnet_finetuning.py
@@ -1,129 +1,42 @@
-# #%#% [markdown]
-# <a href="https://colab.research.google.com/github/gazalssr/HerdNet/blob/main/Base_Herdnet_Finetunning.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
-
-# #%#% [markdown]
-# # DEMO - Training and testing HerdNet on nadir aerial images
-
-# #%#% [markdown]
-# ## Installations
+#Turn on GPU access
+import os
+os.system('nvidia-smi')
 
 
-# #%#%
+# Dataset paths
+#train
+train_root_path='/home/ghazaleh/HerdNet/data/train_m'
+train_gt_path='/home/ghazaleh/HerdNet/data/train_m/train.csv'
+#Validation
+val_root_path='/home/ghazaleh/HerdNet/data/val_m'
+val_gt_path='/home/ghazaleh/HerdNet/data/val_m/val.csv'
+#test
+test_root_path='/home/ghazaleh/HerdNet/data/test_m'
+test_gt_path='/home/ghazaleh/HerdNet/data/test_m/test.csv'
 
 
-
-# #%#% [markdown]
-# ## Create datasets
-
-# #%#%
-# Download some of the data of Delplanque et al. (2021) as an example
-#!gdown 1z9cg2eQs9Xy32ug_OJcbDXIE6A-pEwQM -O /content/data.zip
-#!unzip -oq /content/data.zip -d /content
-
-# #%#%
-# Set the seed
+#Set the seed
 from animaloc.utils.seed import set_seed
-
 set_seed(9292)
 
-# #%#%
-#!python /herdnet/tools/patcher.py -h
 
-# #%#%
-# From bbox to points (no need to do this)
-import pandas as pd
-
-train_df = pd.read_csv('/content/data/train.csv')
-train_df['x'] = (train_df['x_min'] + train_df['x_max']) / 2
-train_df['y'] = (train_df['y_min'] + train_df['y_max']) / 2
-train_df = train_df[['images','x','y','labels']]
-train_df.to_csv('/content/data/train.csv', index=False)
-
-val_df = pd.read_csv('/content/data/val.csv')
-val_df['x'] = (val_df['x_min'] + val_df['x_max']) / 2
-val_df['y'] = (val_df['y_min'] + val_df['y_max']) / 2
-val_df = val_df[['images','x','y','labels']]
-val_df.to_csv('/content/data/val.csv', index=False)
-
-# #%#%
+#Showing an image that is patched
 #%matplotlib inline
-import matplotlib.pyplot as plt
+import pandas as pd
 import PIL.Image as Image
-
+import matplotlib.pyplot as plt
 from animaloc.data.patches import ImageToPatches, AnnotatedImageToPatches
-
-img_path = '/content/drive/MyDrive/images/CAH_GRP_1.jpg'
-
-sample_img = Image.open(img_path)
-
+img_path ='/home/ghazaleh//HerdNet/whole_images_stratified/train/CAH_5_50_2017.jpg'
+sample_img=Image.open(img_path)
 patcher = ImageToPatches(sample_img, (512,512), overlap = 100)
 patches = patcher.make_patches()
-
 print(f'Number of patches: {len(patches)}')
-
 plt.figure(figsize=(15,10))
 _ = patcher.show()
 
-# #%#%
-# Create training  patches using the patcher tool
-from animaloc.utils.useful_funcs import mkdir
-import pandas as pd
-df = pd.read_csv('/content/drive/MyDrive/CAH_small_samples/Train_CAH_Sample/Train_CAH_Sample.csv')
-df = df.to_csv('/content/drive/MyDrive/CAH_small_samples/Train_CAH_Sample/Train_CAH_Sample.csv', index=False)
-# mkdir('/content/drive/MyDrive/Train_patches')
-mkdir('/content/Train_patches') # local directory
-# !python /content/HerdNet/tools/patcher.py /content/drive/MyDrive/images/ 512 512 256 /content/Train_patches -csv /content/drive/MyDrive/images/TRAIN_FCH_8.csv -min 0.0 -all True
-#!python /content/HerdNet/tools/patcher.py /content/drive/MyDrive/CAH_small_samples/Train_CAH_Sample/ 512 512 256 /content/Train_patches -csv /content/drive/MyDrive/CAH_small_samples/Train_CAH_Sample/Train_CAH_Sample.csv -min 0.0 -all False
 
-
-# #%#%
-# Create validation patches
-#mkdir('/content/drive/MyDrive/val_patches')
-mkdir('/content/val_patches')
-# !python /content/HerdNet/tools/patcher.py /content/drive/MyDrive/valFCH/ 512 512 256 /content/validation_patches -csv /content/drive/MyDrive/valFCH/FCH_val_8.csv -min 0.0 -all True
-#!python /content/HerdNet/tools/patcher.py /content/drive/MyDrive/CAH_small_samples/Val_CAH_Sample/ 512 512 256 /content/val_patches -csv /content/drive/MyDrive/CAH_small_samples/Val_CAH_Sample/VAL_CAH_Sample.csv -min 0.0 -all False
-
-# #%#%
-from google.colab import drive
-drive.mount('/content/drive')
-
-# #%#%
-# Create test patches
-from animaloc.utils.useful_funcs import mkdir
-import pandas as pd
-mkdir('/content/drive/MyDrive/test_patches')
-# mkdir('/content/test_patches')
-# !python /content/HerdNet/tools/patcher.py /content/drive/MyDrive/CAH_training_session_1/TestCAH 512 512 256 /content/drive/MyDrive/test_patches -csv /content/drive/MyDrive/CAH_training_session_1/TestCAH/TestCAH_2017_1.csv -min 0.0 -all True
-#!python /content/HerdNet/tools/patcher.py /content/drive/MyDrive/CAH_small_samples/Test_CAH_Sample/ 512 512 256 /content/test_patches -csv /content/drive/MyDrive/CAH_small_samples/Test_CAH_Sample/Test_CAH_Sample.csv -min 0.0 -all False
-
-# #%#% [markdown]
-# Ziping the patches and transfer to google drive
-
-# #%#%
-#make a zip file from the patch folders (Only run this when ypou add a new dataset)
-#Zip file path (destination)       source file path
-import shutil
-shutil.make_archive('/content/drive/MyDrive/Train_patches_S', 'zip', '/content/drive/MyDrive/Train_patches')
-shutil.make_archive('/content/drive/MyDrive/val_patches_S', 'zip', '/content/drive/MyDrive/val_patches')
-shutil.make_archive('/content/drive/MyDrive/test_patches_S', 'zip', '/content/drive/MyDrive/test_patches')
-
-# #%#%
-#### Downloading and unziping the files
-#zip file download (destination link)                  Zip file saving location (same as the link)
-#!gdown https://drive.google.com/uc?id=1-QF9YQHWrlfFxeQcRWYhU0cljm4CvoVz -O /content/Train_patches_S.zip
-#Zip file location           uzipped file saving location
-#!unzip -oq /content/Train_patches_S.zip -d /content/Train_patches
-
-#!gdown https://drive.google.com/uc?id=18OfeLVDbgGFAeo09LAd8zMyD16kTLAQ0 -O /content/Val_patches_S.zip
-#!unzip -oq /content/Val_patches_S.zip -d /content/val_patches
-
-#!gdown https://drive.google.com/uc?id=1-79l91SDG5nCiohdvhbIzar3Zab_r60e -O /content/test_patches_S.zip
-#!unzip -oq /content/test_patches_S.zip -d /content/test_patches
-
-
-# #%#%
-#%matplotlib inline
 # Showing some samples of patches and the annotations
+#%matplotlib inline
 import matplotlib.pyplot as plt
 from animaloc.datasets import CSVDataset
 from animaloc.data.batch_utils import show_batch, collate_fn
@@ -131,12 +44,10 @@ from torch.utils.data import DataLoader
 import torch
 import albumentations as A
 batch_size = 8
-
-csv_path = '/content/Train_patches/gt.csv'
-image_path = '/content/Train_patches'
-dataset = CSVDataset(csv_path, image_path, [A.Normalize()])
+csv_path = train_gt_path
+image_path = train_root_path
+dataset = FolderDataset(csv_path, image_path, [A.Normalize()])
 dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=True)
-
 sample_batch = next(iter(dataloader))
 for i in range(len(sample_batch[1])):
   points = sample_batch[1][i]['points'].numpy()
@@ -148,22 +59,19 @@ for i in range(len(sample_batch[1])):
 plt.figure(figsize=(16,2))
 show_batch(sample_batch)
 
-# #%#%
+
 # Training, validation and test datasets
 import albumentations as A
-
-from animaloc.datasets import CSVDataset
+from animaloc.datasets import FolderDataset
 from animaloc.data.transforms import MultiTransformsWrapper, DownSample, PointsToMask, FIDT
 
 patch_size = 512
 num_classes = 2
 down_ratio = 2
 
-train_dataset = CSVDataset(
-    # csv_file = '/content/drive/MyDrive/Train_patches/gt.csv',
-    # root_dir = '/content/drive/MyDrive/Train_patches',
-    csv_file = '/content/Train_patches/gt.csv',
-    root_dir = '/content/Train_patches',
+train_dataset = FolderDataset(
+    csv_file = train_gt_path,
+    root_dir = train_root_path,
     # Data Augmentation
     albu_transforms = [
         A.VerticalFlip(p=0.5),
@@ -171,7 +79,7 @@ train_dataset = CSVDataset(
         A.Normalize(p=1.0),
         A.RandomRotate90(p=0.5),
         A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.2),
-        # A.Blur(blur_limit=15, p=0.2),
+        A.Blur(blur_limit=15, p=0.2),
         ],
     end_transforms = [MultiTransformsWrapper([
         FIDT(num_classes=num_classes, down_ratio=down_ratio),
@@ -179,55 +87,50 @@ train_dataset = CSVDataset(
         ])]
     )
 
-val_dataset = CSVDataset(
-    # csv_file = '/content/drive/MyDrive/val_patches/gt.csv',
-    # root_dir = '/content/drive/MyDrive/val_patches',
-    csv_file = '/content/val_patches/gt.csv',
-    root_dir = '/content/val_patches',
+val_dataset = FolderDataset(
+
+    csv_file = val_gt_path,
+    root_dir = val_root_path,
     albu_transforms = [A.Normalize(p=1.0)],
     end_transforms = [DownSample(down_ratio=down_ratio, anno_type='point')]
     )
 
-test_dataset = CSVDataset(
-    # csv_file = '/content/drive/MyDrive/test_patches/gt.csv',
-    # root_dir = '/content/drive/MyDrive/test_patches',
-    csv_file = '/content/test_patches/gt.csv',
-    root_dir = '/content/test_patches',
+test_dataset = FolderDataset(
+    csv_file = test_gt_path,
+    root_dir = test_root_path,
     albu_transforms = [A.Normalize(p=1.0)],
     end_transforms = [DownSample(down_ratio=down_ratio, anno_type='point')]
     )
 
-# #%#%
+
 # Dataloaders
 from torch.utils.data import DataLoader
 
-train_dataloader = DataLoader(dataset = train_dataset, batch_size = 4, shuffle = True)
+train_dataloader = DataLoader(dataset = train_dataset, batch_size = 1, shuffle = True)
 
 val_dataloader = DataLoader(dataset = val_dataset, batch_size = 1, shuffle = False)
 
 test_dataloader = DataLoader(dataset = test_dataset, batch_size = 1, shuffle = False)
 
-# #%#%
-sample_batch[0].size()
 
-
-# #%% [markdown]
-# ## Define HerdNet for training
-
-# %%
-# Path to your .pth file (initial pth file)
-import gdown
+# Define HerdNet for training
+# 1. Path to your .pth file (initial pth file)
 import torch
-pth_path = 'output/best_model.pth'
+pth_path = '/home/ghazaleh/HerdNet/pth_files/Binary.pth'
+pretrained_dict=torch.load(pth_path)
 
-if not pth_path:
-    gdown.download(
-        'https://drive.google.com/uc?export=download&id=1-WUnBC4BJMVkNvRqalF_HzA1_pRkQTI_',
-        '/content/20220413_herdnet_model.pth'
-        )
-    pth_path = '/content/20220413_herdnet_model.pth'
 
-# %%
+# 2. Transfer weights to Herdnet ###
+from animaloc.models import HerdNet
+herdnet = HerdNet(num_classes=num_classes, down_ratio=down_ratio).cuda()
+herdnet_dict = herdnet.state_dict()
+# Match and load pre-trained weights
+pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in herdnet_dict}
+herdnet_dict.update(pretrained_dict)
+herdnet.load_state_dict(herdnet_dict)
+
+
+#3. Define loss functions
 from animaloc.models import HerdNet
 from torch import Tensor
 from animaloc.models import LossWrapper
@@ -235,7 +138,7 @@ from animaloc.train.losses import FocalLoss
 from torch.nn import CrossEntropyLoss
 
 herdnet = HerdNet(num_classes=num_classes, down_ratio=down_ratio).cuda()
-
+weight = Tensor([1.28,4.54]).cuda()
 losses = [
     {'loss': FocalLoss(reduction='mean'), 'idx': 0, 'idy': 0, 'lambda': 1.0, 'name': 'focal_loss'},
     {'loss': CrossEntropyLoss(reduction='mean'), 'idx': 1, 'idy': 1, 'lambda': 1.0, 'name': 'ce_loss'}
@@ -244,8 +147,7 @@ losses = [
 herdnet = LossWrapper(herdnet, losses=losses)
 
 
-# %%
-#############Get model layers ###########################
+# 4. Get model layers ###########################
 def get_parameter_names(model): # getting the model layers
   param_dict= dict()
   for l, (name,param) in enumerate(model.named_parameters()):
@@ -255,16 +157,23 @@ def get_parameter_names(model): # getting the model layers
 result = get_parameter_names(herdnet)
 print(result)
 
-# %%
-#Freeze the layers
-def freeze_parts(model, get_parameter_names, layers_to_freeze, lr, unfreeze=False):
+
+
+# 5. Freeze half of a specified layer
+def freeze_parts(model, get_parameter_names, layers_to_freeze, freeze_layer_half=None, lr=0.0001, unfreeze=False):
     params_to_update = []
 
     for l, (name, param) in enumerate(model.named_parameters()):
         res = any(ele in name for ele in layers_to_freeze)
         param.requires_grad = unfreeze if res else not unfreeze
 
-        if param.requires_grad == True:
+        # Check if the current layer is the specified layer to freeze half of its parameters
+        if freeze_layer_half is not None and freeze_layer_half in name:
+            total_params = param.numel()
+            half_params = total_params // 2
+            param.requires_grad = unfreeze if l < half_params else not unfreeze
+
+        if param.requires_grad:
             params_to_update.append({
                 "params": param,
                 "lr": lr,
@@ -278,27 +187,18 @@ def freeze_parts(model, get_parameter_names, layers_to_freeze, lr, unfreeze=Fals
 
     return params_to_update
 
-# %%
-#Selecting the freezed layers (change the freezed ayers and record the results)
-lr = 1e-3 # learning rate
-# layers_to_freeze= [] # nothing frozen
-layers_to_freeze= ['base_layer','level0','level1','level2','level3','level4'] # we are feezing all the levels below level5
-# layers_to_freeze= ['base_layer','level0','level1','level2','level3','level4','level5','fc','bottleneck_conv'] # we are feezing everything except cls_head
 
-params_to_update= freeze_parts(herdnet.model,get_parameter_names,layers_to_freeze,lr,unfreeze=False)
-# optimizer = Adam(params=params_to_update, lr=lr, weight_decay=weight_decay)
+# 6. Specifying half of one layer+ other layers to freeze
+params_to_update = freeze_parts(herdnet.model, get_parameter_names, layers_to_freeze=['base_layer','level0','level1','level2','level3'], freeze_layer_half='level_4', lr=0.0001, unfreeze=False)
 
 
-# %% [markdown]
-# ## Create the Trainer
-
-# %%
+# 7. Create the trainer
 from torch.optim import Adam
 from animaloc.train import Trainer
 from animaloc.eval import PointsMetrics, HerdNetStitcher, HerdNetEvaluator
 from animaloc.utils.useful_funcs import mkdir
 
-work_dir = '/content/drive/MyDrive/output'
+work_dir = '/home/ghazaleh/HerdNet/output'
 mkdir(work_dir)
 
 lr = 1e-4
@@ -334,150 +234,58 @@ trainer = Trainer(
     evaluator=evaluator,  # metric evaluation (original)
     # val_dataloader= val_dataloader, # loss evaluation
     work_dir=work_dir
-    )
+ )
 
-# %%
-#!ls /content/drive/MyDrive/val_patches/*.jpg
-# csv_data= pd.read_csv('/content/drive/MyDrive/Train_patches/gt.csv')
-# print(csv_data)
 
-# %% [markdown]
-# ## Inintial training
-
-# %%
+# 8. Start training and saving parameters
+#wandb
 import wandb
 if wandb.run is not None:
   wandb.finish()
 wandb.init(project="herdnet-finetuning")
 
-# %%
+# 9. Trainer
 trainer.start(warmup_iters=100, checkpoints='best', select='max', validate_on='f1_score', wandb_flag =True)
 
-# %%
-##Unfreeze the layers
-layers_to_unfreeze = ['level5', 'fc']
-params_to_update_UF = freeze_parts(herdnet.model, get_parameter_names, layers_to_unfreeze, lr=1e-3, unfreeze=True)
 
-# %%
-work_dir = '/content/drive/MyDrive/output'
-mkdir(work_dir)
-
-lr = 1e-3
-weight_decay = 1e-3
-epochs = 1
-parameters=params_to_update_UF
-# optimizer = Adam(params=params_to_update_UF, lr=lr, weight_decay=)
-optimizer = Adam(params=herdnet.parameters(), lr=lr, weight_decay=weight_decay)
-
-
-metrics = PointsMetrics(radius=20, num_classes=num_classes)
-
-stitcher = HerdNetStitcher(
-    model=herdnet,
-    size=(patch_size,patch_size),
-    overlap=0,
-    down_ratio=down_ratio,
-    reduction='mean'
-    )
-
-evaluator = HerdNetEvaluator(
-    model=herdnet,
-    dataloader=val_dataloader,
-    metrics=metrics,
-    stitcher=None,
-    work_dir=work_dir,
-    header='validation'
-    )
-
-trainer = Trainer(
-    model=herdnet,
-    train_dataloader=train_dataloader,
-    optimizer=optimizer,
-    num_epochs=epochs,
-    evaluator=evaluator,
-    work_dir=work_dir
-    )
-
-# %% [markdown]
-# ## continue traing with unfreezing the freezed layers
-# 
-
-# %%
-import wandb
-if wandb.run is not None:
-  wandb.finish()
-wandb.init(project="herdnet")
-
-# %%
-trainer.start(warmup_iters=100, checkpoints='best', select='max', validate_on='accuracy', wandb_flag =True)
-
-
-# %%
-torch.save(herdnet.state_dict(), '/content/drive/MyDrive/fine_tuned_base_herdnet.pth')
-
-# %% [markdown]
-# ## Test the model
-
-# %%
-# Path to your .pth file
-import gdown
-
-pth_path = '/content/drive/MyDrive/fine_tuned_base_herdnet.pth'
-
-# if not pth_path:
-#     gdown.download(
-#         'https://drive.google.com/uc?export=download&id=1-WUnBC4BJMVkNvRqalF_HzA1_pRkQTI_',
-#         '/content/20220413_herdnet_model.pth'
-#         )
-#     pth_path = '/content/20220413_herdnet_model.pth'
-
-# %%
-# Create output folder
-from animaloc.utils.useful_funcs import mkdir
-test_dir = '/content/drive/MyDrive/test_output_base_finetune'
-mkdir(test_dir)
-
-# %%
- # Load trained parameters
+########## Evaluating the model on training set
+# 1. Load trained parameters
+pth_path='/home/ghazaleh/HerdNet/output/best_model.pth'
 from animaloc.models import load_model
-pth_path= '/content/drive/MyDrive/output/best_model.pth'
 herdnet = load_model(herdnet, pth_path=pth_path)
+torch.save(herdnet.state_dict(), 'fine_tuned_model.pth')  
 
-# %%
-# Create an Evaluator
-test_evaluator = HerdNetEvaluator(
-    model=herdnet,
-    dataloader=test_dataloader,
-    metrics=metrics,
-    stitcher=stitcher,
-    work_dir=test_dir,
-    header='test'
-    )
+# #Define a new train dataloader for evaluation
+# train_dataset = FolderDataset(
 
+#     csv_file = train_gt_path,
+#     root_dir = train_root_path,
+#     # Data Augmentation
+#     albu_transforms = [A.Normalize(p=1.0)],
+#     end_transforms = [DownSample(down_ratio=down_ratio, anno_type='point')]
+# )
 
-# %%
-# Start testing
-test_f1_score = test_evaluator.evaluate(returns='f1_score')
-
-# %%
-# Print global F1 score (%)
-print(f"F1 score = {test_f1_score * 100:0.0f}%")
-
-# %%
-# Get the detections (Metrics)
-detections = test_evaluator.results
-detections.to_csv('/content//drive/MyDrive/test_output_base_finetune/metrics.csv')
-
-# %%
-# precision-recall curves
-#%matplotlib inline
-from animaloc.vizual import PlotPrecisionRecall
-pr_curve = PlotPrecisionRecall(legend=True)
-metrics = test_evaluator._stored_metrics
-for c in range(1, metrics.num_classes):
-    rec, pre = metrics.rec_pre_lists(c)
-    pr_curve.feed(rec, pre, str(c))
-
-pr_curve.plot()
+# train_dataloader = DataLoader(dataset = train_dataset, batch_size = 1, shuffle = False)
+# train_evaluator = HerdNetEvaluator(
+#     model=herdnet,
+#     dataloader=train_dataloader,
+#     metrics=metrics,
+#     stitcher=None,
+#     work_dir=train_dir,
+#     header='train'
+#     )
 
 
+# # Start train evaluation
+# train_f1_score = train_evaluator.evaluate(returns='f1_score')
+
+
+# #Get evaluation results
+# df= train_evaluator.results # getting the validation results in pandas format
+# df.head()
+# df= train_evaluator.detections
+# df.head()
+# df.to_csv('/home/ghazaleh/HerdNet/output/train_detections.csv', index=False)
+
+# #Plot thge detections on the patches...........
+# # from animaloc.data.batch_utils import 
